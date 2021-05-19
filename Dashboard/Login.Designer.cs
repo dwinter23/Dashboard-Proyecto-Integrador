b3c2540a8ce6fbf4cd51aaeb88980cbcdc5ebc2b
JHOSUA DANY DAGA VERASTEGUI <A20102271@idat.edu.pe>
2021-05-18 23:39:03 -0700

    Added the Login for the project.

--- Dashboard/Login.Designer.cs
+++ Dashboard/Login.Designer.cs
@@ -1,17 +1,16 @@
-﻿
-namespace Dashboard
+﻿namespace LOGIN_COMPUTEL
 {
-    partial class Login
+    partial class fmrUno
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,30 +20,190 @@ namespace Dashboard
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmrUno));
+            this.lblUno = new System.Windows.Forms.Label();
+            this.lblDos = new System.Windows.Forms.Label();
+            this.lblTres = new System.Windows.Forms.Label();
+            this.textBoxUno = new System.Windows.Forms.TextBox();
+            this.textBoxDos = new System.Windows.Forms.TextBox();
+            this.buttonUno = new System.Windows.Forms.Button();
+            this.linkLabelUno = new System.Windows.Forms.LinkLabel();
+            this.pictureBoxDos = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTres = new System.Windows.Forms.PictureBox();
+            this.buttonDos = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTres)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Login
+            // lblUno
+            // 
+            this.lblUno.AutoSize = true;
+            this.lblUno.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUno.ForeColor = System.Drawing.Color.Coral;
+            this.lblUno.Location = new System.Drawing.Point(470, 26);
+            this.lblUno.Name = "lblUno";
+            this.lblUno.Size = new System.Drawing.Size(202, 25);
+            this.lblUno.TabIndex = 1;
+            this.lblUno.Text = "¡Iniciar Sesión Aqui!";
+            // 
+            // lblDos
+            // 
+            this.lblDos.AutoSize = true;
+            this.lblDos.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDos.ForeColor = System.Drawing.Color.Coral;
+            this.lblDos.Location = new System.Drawing.Point(433, 87);
+            this.lblDos.Name = "lblDos";
+            this.lblDos.Size = new System.Drawing.Size(59, 20);
+            this.lblDos.TabIndex = 2;
+            this.lblDos.Text = "Usuario";
+            // 
+            // lblTres
+            // 
+            this.lblTres.AutoSize = true;
+            this.lblTres.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTres.ForeColor = System.Drawing.Color.Coral;
+            this.lblTres.Location = new System.Drawing.Point(433, 177);
+            this.lblTres.Name = "lblTres";
+            this.lblTres.Size = new System.Drawing.Size(83, 20);
+            this.lblTres.TabIndex = 3;
+            this.lblTres.Text = "Contraseña";
+            // 
+            // textBoxUno
+            // 
+            this.textBoxUno.BackColor = System.Drawing.Color.MistyRose;
+            this.textBoxUno.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUno.Location = new System.Drawing.Point(449, 132);
+            this.textBoxUno.Name = "textBoxUno";
+            this.textBoxUno.Size = new System.Drawing.Size(247, 13);
+            this.textBoxUno.TabIndex = 4;
+            // 
+            // textBoxDos
+            // 
+            this.textBoxDos.BackColor = System.Drawing.Color.MistyRose;
+            this.textBoxDos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxDos.Location = new System.Drawing.Point(449, 229);
+            this.textBoxDos.Name = "textBoxDos";
+            this.textBoxDos.Size = new System.Drawing.Size(247, 13);
+            this.textBoxDos.TabIndex = 5;
+            this.textBoxDos.TextChanged += new System.EventHandler(this.textBoxDos_TextChanged);
+            // 
+            // buttonUno
+            // 
+            this.buttonUno.BackColor = System.Drawing.Color.Coral;
+            this.buttonUno.Location = new System.Drawing.Point(449, 295);
+            this.buttonUno.Name = "buttonUno";
+            this.buttonUno.Size = new System.Drawing.Size(247, 52);
+            this.buttonUno.TabIndex = 6;
+            this.buttonUno.Text = "INICIAR SESION";
+            this.buttonUno.UseVisualStyleBackColor = false;
+            // 
+            // linkLabelUno
+            // 
+            this.linkLabelUno.AutoSize = true;
+            this.linkLabelUno.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelUno.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(91)))), ((int)(((byte)(101)))), ((int)(((byte)(74)))));
+            this.linkLabelUno.Location = new System.Drawing.Point(495, 364);
+            this.linkLabelUno.Name = "linkLabelUno";
+            this.linkLabelUno.Size = new System.Drawing.Size(157, 17);
+            this.linkLabelUno.TabIndex = 7;
+            this.linkLabelUno.TabStop = true;
+            this.linkLabelUno.Text = "¿Olvidaste tu contraseña?";
+            // 
+            // pictureBoxDos
+            // 
+            this.pictureBoxDos.BackColor = System.Drawing.Color.MistyRose;
+            this.pictureBoxDos.Location = new System.Drawing.Point(437, 121);
+            this.pictureBoxDos.Name = "pictureBoxDos";
+            this.pictureBoxDos.Size = new System.Drawing.Size(273, 39);
+            this.pictureBoxDos.TabIndex = 8;
+            this.pictureBoxDos.TabStop = false;
+            this.pictureBoxDos.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBoxTres
+            // 
+            this.pictureBoxTres.BackColor = System.Drawing.Color.MistyRose;
+            this.pictureBoxTres.Location = new System.Drawing.Point(437, 217);
+            this.pictureBoxTres.Name = "pictureBoxTres";
+            this.pictureBoxTres.Size = new System.Drawing.Size(273, 39);
+            this.pictureBoxTres.TabIndex = 9;
+            this.pictureBoxTres.TabStop = false;
+            // 
+            // buttonDos
+            // 
+            this.buttonDos.BackColor = System.Drawing.Color.Coral;
+            this.buttonDos.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.buttonDos.Location = new System.Drawing.Point(706, 12);
+            this.buttonDos.Name = "buttonDos";
+            this.buttonDos.Size = new System.Drawing.Size(26, 23);
+            this.buttonDos.TabIndex = 10;
+            this.buttonDos.Text = "X";
+            this.buttonDos.UseVisualStyleBackColor = false;
+            this.buttonDos.Click += new System.EventHandler(this.buttonDos_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(72, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 226);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fmrUno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(622, 302);
-            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(744, 401);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonDos);
+            this.Controls.Add(this.linkLabelUno);
+            this.Controls.Add(this.buttonUno);
+            this.Controls.Add(this.textBoxDos);
+            this.Controls.Add(this.textBoxUno);
+            this.Controls.Add(this.lblTres);
+            this.Controls.Add(this.lblDos);
+            this.Controls.Add(this.lblUno);
+            this.Controls.Add(this.pictureBoxDos);
+            this.Controls.Add(this.pictureBoxTres);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Login";
-            this.Text = "Login";
+            this.Name = "fmrUno";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.fmrUno_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.fmrUno_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTres)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label lblUno;
+        private System.Windows.Forms.Label lblDos;
+        private System.Windows.Forms.Label lblTres;
+        private System.Windows.Forms.TextBox textBoxUno;
+        private System.Windows.Forms.TextBox textBoxDos;
+        private System.Windows.Forms.Button buttonUno;
+        private System.Windows.Forms.LinkLabel linkLabelUno;
+        private System.Windows.Forms.PictureBox pictureBoxDos;
+        private System.Windows.Forms.PictureBox pictureBoxTres;
+        private System.Windows.Forms.Button buttonDos;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
+
--- Dashboard/Login.cs
+++ Dashboard/Login.cs
@@ -8,13 +8,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Dashboard
+namespace LOGIN_COMPUTEL
 {
-    public partial class Login : Form
+    public partial class fmrUno : Form
     {
-        public Login()
+        public fmrUno()
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fmrUno_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDos_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBoxDos_TextChanged(object sender, EventArgs e)
+        {
+        }
+        int posY = 0;
+        int posX = 0;
+        private void fmrUno_MouseMove(object sender, MouseEventArgs e)
+        {
+        if(e.Button!= MouseButtons.Left)
+        {
+            posX = e.X;
+            posY = e.Y;
+        }
+        else
+        {
+            Left = Left + (e.X - posX);
+            Top = Top + (e.Y - posY);
+        }
+        
+    }
     }
 }
+
+
+
